1131bcb7b29c678a46129e547f1cf2c002776140
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2023-05-17 22:18:06 +0300

    fist and second task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
-console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems.map (({ preview, original, description}) =>`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>` ).join('');
+
+listGallery.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    const startPlayer = localStorage.getItem("videoplayer-current-time");
+   
+
+    player.on('play', function(timeupdate) {
+        localStorage.setItem("videoplayer-current-time", timeupdate.seconds); 
+    });
+
+
+    if (startPlayer !== null && startPlayer !== undefined){
+        player.setCurrentTime(startPlayer).then(function(seconds) {
+            
+        }).catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                    break;
+                default:
+                    break;
+            }
+        });
+        }
+         
+
+
+
+
+
+
+
+
+  
+   
