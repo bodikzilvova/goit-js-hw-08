8da57a8b277e152477774372ab0b36df3e5e48f8
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2023-05-23 18:01:16 +0300

    fixed bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,25 +5,13 @@ const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
     const startPlayer = localStorage.getItem("videoplayer-current-time");
    
-
-    player.on('play', throttle(function(timeupdate) {
-        localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
-        console.log(timeupdate.seconds);
+    player.on('timeupdate', throttle(function(timeupdate) {
+        const STORAGE_KEY = localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
     }, 1000));
 
 
-
-    if (startPlayer !== null && startPlayer !== undefined){
-        player.setCurrentTime(startPlayer).then(function(seconds) {
-            
-        }).catch(function(error) {
-            switch (error.name) {
-                case 'RangeError':
-                    break;
-                default:
-                    break;
-            }
-        });
+    if (startPlayer){
+        player.setCurrentTime(startPlayer);
         };
          
 
